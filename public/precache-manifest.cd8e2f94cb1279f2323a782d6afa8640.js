57e32409ad79515f9ff0868de5460a0382845d88
eva montgomery <eva.m.montgomery@gmail.com>
2020-03-12 12:16:27 -0400

    adds fe

---
+++ public/precache-manifest.cd8e2f94cb1279f2323a782d6afa8640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1024e9f18410ea66b1f72c3bc308df3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0912f7cf640d4cef34d",
+    "url": "/static/css/main.1b8aff2d.chunk.css"
+  },
+  {
+    "revision": "19be6e60337107799766",
+    "url": "/static/js/2.c2486a0c.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.c2486a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0912f7cf640d4cef34d",
+    "url": "/static/js/main.7c037739.chunk.js"
+  },
+  {
+    "revision": "1ac0d44cbb2004333ad1",
+    "url": "/static/js/runtime-main.7bb9436f.js"
+  }
+]);
